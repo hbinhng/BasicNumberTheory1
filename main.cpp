b01ebf7133c864849bcc54c0ed7061323923142e
beast462 <binhmattroi@gmail.com>
2022-08-27 18:58:24 +0700

    (main) add part to namespaces

--- main.cpp
+++ main.cpp
@@ -2,20 +2,22 @@
 
 using namespace std;
 
-namespace bai1 { void solve(); }
-namespace bai2 { void solve(); }
-namespace bai3 { void solve(); }
-namespace bai4 { void solve(); }
-namespace bai5 { void solve(); }
-namespace bai6 { void solve(); }
-namespace bai7 { void solve(); }
-namespace bai8 { void solve(); }
-namespace bai9 { void solve(); }
-namespace bai10 { void solve(); }
-namespace bai11 { void solve(); }
-namespace bai12 { void solve(); }
+namespace bai1_1 { void solve(); }
+namespace bai1_2 { void solve(); }
+namespace bai1_3 { void solve(); }
+namespace bai1_4 { void solve(); }
+namespace bai1_5 { void solve(); }
+namespace bai1_6 { void solve(); }
+namespace bai1_7 { void solve(); }
+namespace bai1_8 { void solve(); }
+namespace bai1_9 { void solve(); }
+namespace bai1_10 { void solve(); }
+namespace bai1_11 { void solve(); }
+namespace bai1_12 { void solve(); }
+
+namespace bai3_33 { void solve(); }
 
 int main()
 {
-    bai12::solve();
+    bai3::solve();
 }
